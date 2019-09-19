fee260287605497c327c36b72e90bf23c5f2eb78
黄知恒 <huangzhiheng@xiaojubianli.com>
2019-09-19 21:40:06 +0800

    - cock base animation cilp

--- src/component/Cock.js
+++ src/component/Cock.js
@@ -1,7 +1,35 @@
 import * as BABYLON from 'babylonjs'
 
 export default class Cock extends BABYLON.AbstractMesh {
-    constructor(name, scene) {
+    constructor(name, resource, scene) {
         super(name, scene)
+        const { game, asset: { mesh, animation } } = resource
+        this.game = game
+        this.meshes = mesh
+        this.animation = animation
+        mesh.forEach(mesh => {
+            mesh.setEnabled(true)
+            mesh.parent = this
+        })
+        this.setEnabled(false)
+    }
+    init() {
+        this.rotation.y = - Math.PI / 2
+        this.setEnabled(true)
+    }
+    idle() {
+        this.animation.start(true, 0.5, 6.25 * 1/150, 6.25 * 30/150)
+    }
+    walk() {
+        this.animation.start(true, 1, 6.25 * 31/150, 6.25 * 60/150)
+    }
+    fly() {
+        this.animation.start(true, 1, 6.25 * 61/150, 6.25 * 90/150)
+    }
+    jump() {
+        this.animation.start(true, 1.25, 6.25 * 91/150, 6.25 * 120/150)
+    }
+    dead() {
+        this.animation.start(false, 1.25, 6.25 * 121/150, 6.25 * 150/150)
     }
 }
--- src/component/Game.js
+++ src/component/Game.js
@@ -1,7 +1,9 @@
 import * as BABYLON from 'babylonjs'
+import 'babylonjs-loaders'
+
 import AxesHelper from '../helper/AxesHelper'
 import JumpAssetsManager from './JumpAssetsManager'
-import 'babylonjs-loaders'
+import Cock from './Cock'
 
 import skyPic from '../assets/sky.jpg'
 
@@ -13,18 +15,20 @@ export default class Game {
         const ngOptions = null
         const adaptDeviceRatio = true
 
+        this.canvas = canvas
         this.engine = new BABYLON.Engine(canvas, antialias, ngOptions, adaptDeviceRatio)
         this.scene = new BABYLON.Scene(this.engine)
         window.onresize = () => {
             this.engine.resize()
         }
+        this.assets = null
     }
     async init() {
         const alpha = 3 * Math.PI / 2
         const beta = 11 * Math.PI / 20
         const radius = 15
         this.mainCamera = new BABYLON.ArcRotateCamera('main_camera', alpha, beta, radius, new BABYLON.Vector3(1, 6, -2), this.scene)
-        this.mainCamera.attachControl(window)
+        this.mainCamera.attachControl(this.canvas, true)
         this.mainLight = new BABYLON.HemisphericLight('main_Light', new BABYLON.Vector3(0, 2, 0), this.scene)
         new AxesHelper('helper', {
             size: 3
@@ -35,6 +39,13 @@ export default class Game {
         try {
             const jumpAssetsManager = new JumpAssetsManager(this.scene)
             const { assets } = await jumpAssetsManager.load()
+            this.assets = assets
+            this.cock = new Cock('cock', {
+                game: this,
+                asset: this.assets.cock
+            }, this.scene)
+            this.cock.init()
+            this.cock.dead()
         } catch (e) {
             throw e
         }
